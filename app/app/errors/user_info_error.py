cc6d3aa667d27b0e144defb44469b53d807cd4de
Rockford Mankini <rmankini@ucsd.edu>
2022-09-06 16:23:29 -0700

    remove hardcoded base_path

--- app/app/errors/user_info_error.py
+++ app/app/errors/user_info_error.py
@@ -2,6 +2,9 @@ from starlette.requests import Request
 from starlette.responses import RedirectResponse
 import urllib.parse
 
+from app.config.environment import get_settings
+_SETTINGS = get_settings()
+
 class UserInfoException(Exception):
     def __init__(self):
         message = "You aren't logged in! Redirecting to the login page..."
@@ -10,6 +13,8 @@ async def get_user_info_exception_handler(request: Request, exc: UserInfoExcepti
 
 	target = urllib.parse.quote_plus(str(request.url))
 
+	BASE_PATH = _SETTINGS.BASE_PATH
+
 	return(
-	RedirectResponse(url=f"/python-webapp-archetype/saml/login?target={target}")
+	RedirectResponse(url=f"{BASE_PATH}/saml/login?target={target}")
 	)
